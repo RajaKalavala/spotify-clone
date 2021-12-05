8220db323bea5528879ba140d6517487c4e33205
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2021-12-05 15:02:48 +0530

    Update Login.js UI of login page

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,32 @@
-function login() {
+import { getProviders, signIn } from "next-auth/react";
+
+function login({ providers }) {
   return (
-    <div>
-      <h1>This is Login Page</h1>
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
+
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="bg-[#18D860] text-white p-5 rounded-full"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Login With {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
